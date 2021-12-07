c7b9a5ff355fb736de416212961ddfbbcd128c57
Rob Uridge <rob@uridge.com>
2021-12-07 22:42:35 +0800

    wip people page heading+add people button

--- src/pages/People/People.js
+++ src/pages/People/People.js
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
+
+import Text from 'components/Text';
+import Button from 'components/Button';
+
+import { ReactComponent as IconUser } from 'theme/icons/user.svg';
 
 const Container = styled.main`
   margin: 40px auto;
@@ -7,10 +12,30 @@ const Container = styled.main`
   max-width: var(--layout-width);
 `;
 
+const StyledIconUser = styled(IconUser)`
+  fill: white;
+  margin-right: 8px;
+  width: 20px;
+  height: 20px;
+`;
+
 export default function People() {
   return (
     <Container>
-      <p>People page coming...</p>
+      <div
+        css={css`
+          display: flex;
+          justify-content: space-between;
+        `}
+      >
+        <Text as="h1" size="h2">
+          People
+        </Text>
+        <Button>
+          <StyledIconUser />
+          Add member
+        </Button>
+      </div>
       <Link to="/playground">Playground</Link>
     </Container>
   );
